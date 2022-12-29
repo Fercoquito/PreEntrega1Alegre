783cf4efc2253ee0bcdee7af8121035b3e36034a
feralegre85@gmail.com <feralegre85@gmail.com>
2022-12-29 12:39:43 -0300

    Preentrega1

---
+++ src/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Title from "../Title/Title";
+import "./ItemListContainer.css"
+
+export const ItemListContainer = () => {
+  return (
+    <div className='title'> 
+    
+    <Title  greeting = "Bienvenidos al mundo de las camisetas"/>
+    
+    </div>
+  )
+
+}
+
+export default ItemListContainer;
---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,33 @@
+import "./NavBar.css";
+import CartWidget from "../CartWitget/CartWidget";
+
+export default function NavBar() {
+    return (
+      
+      <div>
+        <div className="navBar">
+          <div className="Logo">
+            <img src="/" alt="logo" />
+          </div>
+          <nav>
+            <ul className="nav-links">
+              <li>
+                <a href="/">Camisetas Internacionales</a>
+              </li>
+              <li>
+                <a href="/">Camisetas Nacionales</a>
+              </li>
+              <li>
+                <a href="/">Shorts</a>
+              </li>
+              <li>
+                <a href="/"><CartWidget/></a>
+              </li>
+              
+            </ul>
+          </nav>
+        </div>
+      </div>
+      
+    );
+  }
